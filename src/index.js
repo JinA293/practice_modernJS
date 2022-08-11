ac73f2d222be1a8d842fb7c2e8e9d963741634c3
Jin Saito <jin.codegym1217@gmail.com>
2022-08-11 23:52:04 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -11,3 +11,18 @@ const sayHello = (name = "jin") => {
   return console.log(`Iam${name}`);
 };
 sayHello("akira");
+
+const arr1 = [1, 2, 3, 4, 5];
+console.log(arr1);
+console.log(...arr1);
+
+const sumFunc = (num1, num2) => {
+  return console.log(num1 + num2);
+};
+sumFunc(...arr1);
+
+const [num1, ...arr2] = arr1;
+console.log(arr1);
+console.log(num1);
+// console.log(num2)
+console.log(arr2);
