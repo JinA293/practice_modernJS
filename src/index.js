4e870a9a3d5b2cc090c3103576bdd79a242e5ba6
Jin Saito <jin.codegym1217@gmail.com>
2022-08-12 01:26:49 +0900

    filter, mapFunction

--- src/index.js
+++ src/index.js
@@ -1,35 +1,63 @@
-const me = {
-  name: "jin",
-  age: 20
-};
+// const me = {
+//   name: "jin",
+//   age: 20
+// };
 
 // const { name, age } = me;
 
 // const message = `Iam${name}`;
 
-const sayHello = (name = "jin") => {
-  return console.log(`Iam${name}`);
-};
-sayHello("akira");
+// const sayHello = (name = "jin") => {
+//   return console.log(`Iam${name}`);
+// };
+// sayHello("akira");
 
-const arr1 = [1, 2, 3, 4, 5];
-console.log(arr1);
-console.log(...arr1);
+// const arr1 = [1, 2, 3, 4, 5];
+// console.log(arr1);
+// console.log(...arr1);
 
-const sumFunc = (num1, num2) => {
-  return console.log(num1 + num2);
-};
-sumFunc(...arr1);
+// const sumFunc = (num1, num2) => {
+//   return console.log(num1 + num2);
+// };
+// sumFunc(...arr1);
 
-const [num1, ...arr2] = arr1;
-console.log(arr1);
-console.log(num1);
-// console.log(num2)
-console.log(arr2);
+// const [num1, ...arr2] = arr1;
+// console.log(arr1);
+// console.log(num1);
+// // console.log(num2)
+// console.log(arr2);
 
-const arr3 = [6, 7, 8, 9, 10];
-const arr4 = [...arr3];
-console.log(arr4);
+// const arr3 = [6, 7, 8, 9, 10];
+// const arr4 = [...arr3];
+// console.log(arr4);
 
-const arr5 = [arr1, arr3[0], ...arr2];
-console.log(arr5);
+// const arr5 = [arr1, arr3[0], ...arr2];
+// console.log(arr5);
+
+const nameArr = ["じん", "山田", "中村"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`)
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+
+// console.log(nameArr2)
+
+// nameArr.map((name) => console.log(`${name}です`))
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr)
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "じん") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
